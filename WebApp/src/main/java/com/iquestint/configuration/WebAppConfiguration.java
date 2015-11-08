f4d597da9a9be75ebb286fbca90e9ce2e7cbfc5a
Vladutu Georgian <vladutu_georgian_4d@yahoo.com>
2015-11-08 14:22:40 +0200

    - added a simpleMappingExceptionResolver bean and an error jsp page so that when the db is offline a message is printed instead of the exception stack trace.

--- WebApp/src/main/java/com/iquestint/configuration/WebAppConfiguration.java
+++ WebApp/src/main/java/com/iquestint/configuration/WebAppConfiguration.java
@@ -5,17 +5,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.transaction.CannotCreateTransactionException;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 import java.util.Locale;
+import java.util.Properties;
 
 /**
  * @author vladu
@@ -50,6 +53,21 @@ public class WebAppConfiguration extends WebMvcConfigurerAdapter {
         resolver.setCookieName("myLocaleCookie");
         resolver.setCookieMaxAge(4800);
         return resolver;
+    }
+
+    @Bean
+    public SimpleMappingExceptionResolver simpleMappingExceptionResolver() {
+        SimpleMappingExceptionResolver r =
+            new SimpleMappingExceptionResolver();
+
+        Properties mappings = new Properties();
+        mappings.setProperty(CannotCreateTransactionException.class.getName(), "error");
+
+        r.setExceptionMappings(mappings);  // None by default
+        // r.setDefaultErrorView("error");    // No default
+        r.setExceptionAttribute("ex");     // Default is "exception"
+        r.setWarnLogCategory("example.MvcLogger");     // No default
+        return r;
     }
 
     @Override
